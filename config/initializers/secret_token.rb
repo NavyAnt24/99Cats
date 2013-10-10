e0e6a8ab88be380f3e1756ee935f344b1ca83821
App Academy Student <student@appacademy.io>
2013-10-10 11:16:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cats99::Application.config.secret_token = '151337c29bb65579cc939485350c1172d0e3b3825d242955f838b9cb655ccc39830b0af8af4ae86d863ced3cdb978009e4b14e199c2237616ecd945d5b75bdeb'
